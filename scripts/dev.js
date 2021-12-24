d9e3d848bedc48724cf0291ab606f482991e0ed8
Mohammed Saud <md.saud020@gmail.com>
2021-12-25 00:33:01 +0530

    switch to using esbuild dev server

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,17 +1,23 @@
 const glslxPlugin = require('@rottencandy/esbuild-plugin-glslx');
-require('esbuild').build({
-  entryPoints: ['src/main.js', 'src/app.css'],
-  bundle: true,
-  sourcemap: 'inline',
-  charset: 'utf8',
-  target: 'es6',
-  format: 'iife',
-  outdir: 'app',
-  watch: {
-    onRebuild(error) {
-      if (error) console.error('watch build failed:', error.errors)
-      else console.log('watch build succeeded!')
-    },
-  },
-  plugins: [glslxPlugin({ prettyPrint: true, renaming: 'none' })],
-}).catch(() => process.exit(1))
+const esbuild = require('esbuild');
+
+esbuild.serve({
+    servedir: 'app',
+}, {
+    entryPoints: ['src/main.js', 'src/app.css'],
+    bundle: true,
+    sourcemap: 'inline',
+    charset: 'utf8',
+    target: 'es6',
+    format: 'iife',
+    outdir: 'app',
+    //watch: {
+    //    onRebuild(error) {
+    //        if (error) console.error('Build failed:', error.errors)
+    //        else console.log('Build succeeded!')
+    //    },
+    //},
+    plugins: [glslxPlugin({ prettyPrint: true, renaming: 'none' })],
+})
+    .then(server => console.log(`Serving at: http://localhost:${server.port}`))
+    .catch(() => process.exit(1));
