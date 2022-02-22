8fc39f0b375f3c3323f11b7466f784af1dba49f8
Mohammed Saud <md.saud020@gmail.com>
2022-02-22 21:32:09 +0530

    mangle state machine props

--- src/engine/state.ts
+++ src/engine/state.ts
@@ -10,22 +10,22 @@ type StateObject = {
  *
  * @example
  * const step = createSM({
- *   0: () => {},
- *   1: () => {}
+ *   IDLE: () => {},
+ *   MOVE: () => {}
  * });
  *
- * @returns `run`: Step function, returns last run state.
+ * @returns `run_`: Step function, returns last run state.
  * Any args will be passed on to state function.
- * `reset`: reset state to given key.
+ * `reset_`: reset state to given key.
  */
 export const createStateMachine = (states: StateObject, initial: string) => {
     let state = initial;
     return {
-        run: (...data: any[]) => {
+        run_: (...data: any[]) => {
             const next = states[state](...data);
             state = next === undefined ? state : next;
             return state;
         },
-        reset: (x: string) => state = x,
+        reset_: (x: string) => state = x,
     };
 }
