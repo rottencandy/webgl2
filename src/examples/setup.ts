83b75c9762a951c49c1657df0300e82ffa5beafc
Mohammed Saud <md.saud020@gmail.com>
2022-12-31 00:49:08 +0530

    implement example component switcher UI

--- src/examples/setup.ts
+++ src/examples/setup.ts
@@ -4,18 +4,39 @@ import { CompPhysicsRun } from "../engine/components/physics";
 import { CompRenderRun } from "../engine/components/render";
 import { createGLContext, resize } from "../engine/webgl2-stateless";
 import { FPSCam3D } from "./utils/views";
-import { setup as setupGrid } from "./grid";
-import { setup } from "./lightning";
+import { setup as setupGrid, teardown as teardownGrid } from "./grid";
+import { setup as setupLight, teardown as teardownLight } from "./lightning";
+import { addToPanel } from "../debug";
+import { $ } from "../globals";
+
+const scenes
+:{ [key: string]: { setup: (gl: WebGL2RenderingContext) => void, teardown: () => void } } = {
+    grid: { setup: setupGrid, teardown: teardownGrid },
+    lightning: { setup: setupLight, teardown: teardownLight },
+};
+let active = 'grid';
 
 export const runExamples = () => {
+
     const canvas = document.getElementById('c') as HTMLCanvasElement, width = 300, height = 300;
     setupKeyListener(canvas, true);
     const gl = createGLContext(canvas, width, height);
     (onresize = () => resize(gl, canvas, width, height))();
     const cam = FPSCam3D(.01, 0, 5, 20, width / height);
 
-    setup(gl);
-    setupGrid(gl);
+    active && scenes[active].setup(gl);
+    addToPanel(
+        $('select', {
+            name: 'scenes',
+            onchange: (e: any) => {
+                const next = e.target.value;
+                active && scenes[active]?.teardown();
+                active = next;
+                scenes[active].setup(gl);
+            },
+        },
+        ...Object.keys(scenes).map(k => $('option', { value: k }, k))),
+    );
 
     startLoop(
         (dt) => {
