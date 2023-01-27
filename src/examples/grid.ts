79655d909d5617d3812f7953870b6718178627ce
Mohammed Saud <md.saud020@gmail.com>
2023-01-27 21:33:56 +0530

    fix grid depth bug

--- src/examples/grid.ts
+++ src/examples/grid.ts
@@ -1,15 +1,15 @@
 import { mat4 } from 'gl-matrix';
 import { CompRender } from '../components/render';
-import { bindVAO, mesh, shaderProgram, uniformFns, useProgram } from '../core/webgl2-stateless';
+import { bindVAO, getUniformLoc, m4fset, mesh, shaderProgram, useProgram } from '../core/webgl2-stateless';
 import { Plane } from '../vertices';
 
 const vert = `#version 300 es
 precision lowp float;
 layout(location=0)in vec2 aPos;
-uniform mat4 uMat;
+uniform mat4 uVPMat;
 out vec3 nearP;
 out vec3 farP;
-out mat4 fragMat;
+out mat4 VPMat;
 
 vec3 unproject(float x, float y, float z, mat4 proj) {
     vec4 point = inverse(proj) * vec4(x, y, z, 1.);
@@ -19,9 +19,9 @@ vec3 unproject(float x, float y, float z, mat4 proj) {
 void main() {
     // -1 -> 1
     vec2 p = aPos - 1.;
-    nearP = unproject(p.x, p.y, 0., uMat);
-    farP = unproject(p.x, p.y, 1., uMat);
-    fragMat = uMat;
+    nearP = unproject(p.x, p.y, 0., uVPMat);
+    farP = unproject(p.x, p.y, 1., uVPMat);
+    VPMat = uVPMat;
     gl_Position = vec4(p, 0., 1.);
 }`;
 
@@ -29,11 +29,11 @@ const frag = `#version 300 es
 precision lowp float;
 in vec3 nearP;
 in vec3 farP;
-in mat4 fragMat;
+in mat4 VPMat;
 out vec4 fragColor;
 
-vec4 grid(vec3 fragPos3D, float scale) {
-    vec2 coord = fragPos3D.xz * scale;
+vec4 grid(vec3 fragPos3D) {
+    vec2 coord = fragPos3D.xz;
     vec2 derivative = fwidth(coord);
     vec2 grid = abs(fract(coord - 0.5) - 0.5) / derivative;
     float line = 1. - min(grid.x, grid.y);
@@ -46,25 +46,27 @@ vec4 grid(vec3 fragPos3D, float scale) {
 }
 
 float computeDepth(vec3 p) {
-    vec4 clipSpacePos = fragMat * vec4(p.xyz, 1.);
-    return clipSpacePos.w;
+    vec4 clipPos = VPMat * vec4(p.xyz, 1.);
+    float depth = clipPos.z / clipPos.w;
+    return depth * .5 + .5;
 }
 
 void main() {
+    // compute pixel pos using slope
     float t = -nearP.y / (farP.y - nearP.y);
     vec3 fragPos3D = nearP + t * (farP - nearP);
 
     gl_FragDepth = computeDepth(fragPos3D);
 
-    fragColor = grid(fragPos3D, .1) * step(0., t) * smoothstep(.5, .0, t);
+    fragColor = grid(fragPos3D) * step(0., t) * smoothstep(.5, .0, t);
 }
 `;
 
-let vao: WebGLVertexArrayObject, prg: WebGLProgram, draw: () => void, uniform: any, loaded = false;
+let vao: WebGLVertexArrayObject, prg: WebGLProgram, draw: () => void, uVPMat: WebGLUniformLocation, loaded = false;
 const render = (gl: WebGL2RenderingContext, mat: mat4) => {
     bindVAO(gl, vao);
     useProgram(gl, prg);
-    uniform('uMat').m4fv(mat);
+    m4fset(gl, uVPMat, mat);
     draw();
 };
 
@@ -72,7 +74,7 @@ export const enableGrid = (gl: WebGL2RenderingContext) => {
     CompRender.push(render);
     if (loaded) return;
     prg = shaderProgram(gl, vert, frag);
-    uniform = uniformFns(gl, prg);
+    uVPMat = getUniformLoc(gl, prg, 'uVPMat');
     [vao, draw] = mesh(gl, Plane(2), [[0, 2]]);
     loaded = true;
 };
