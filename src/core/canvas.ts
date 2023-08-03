337aaaae3817e3a740c5dc12ae468ccf1090f3ba
Mohammed Saud <md.saud020@gmail.com>
2023-08-04 00:15:25 +0530

    add 2d canvas to texture util

--- src/core/canvas.ts
+++ src/core/canvas.ts
@@ -1,3 +1,5 @@
+import { $ } from '../core/ui';
+import { GL, setTextureFilter, setTextureImage, setTextureWrap, texture } from './webgl2-stateless';
 type CTX = CanvasRenderingContext2D;
 
 export const create2dContext = (
@@ -21,4 +23,30 @@ export const resize2d = (canvas: HTMLCanvasElement, width: number, height: numbe
 
 export const clear2d = (ctx: CTX, width: number, height: number) => {
     ctx.clearRect(0, 0, width, height);
+};
+
+// offscreen canvas for making textures
+const offCanvas = $('canvas') as HTMLCanvasElement;
+const oCtx = offCanvas.getContext('2d') as CTX;
+
+// Draw a pixel to raw canvas data
+const drawPixel = (imgData: ImageData, width: number, x: number, y: number, r: number, g: number, b: number, a: number) => {
+    const index = (x + y * width) * 4;
+
+    imgData.data[index + 0] = r;
+    imgData.data[index + 1] = g;
+    imgData.data[index + 2] = b;
+    imgData.data[index + 3] = a;
+};
+
+export const makeCanvasTexture = (width: number, height: number) => {
+    offCanvas.width = width;
+    offCanvas.height = height;
+    clear2d(oCtx, width, height);
+    const imgData = oCtx.getImageData(0, 0, width, height);
+    drawPixel(imgData, width, 0, 0, 255, 0, 0, 255);
+    drawPixel(imgData, width, 1, 0, 0, 255, 0, 255);
+    drawPixel(imgData, width, 0, 1, 0, 0, 255, 255);
+    oCtx.putImageData(imgData, 0, 0);
+    return offCanvas;
 };
